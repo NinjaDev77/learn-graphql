85c72159441ceb2934818d76f19a5bf856af22ba
Ashish Sahani <ashish.sahani@tier5.in>
2019-03-01 16:45:41 +0530

    input type

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga'
+import uuidv4 from 'uuid/v4'
 
 // Scalar types - String, Boolean, Int, Float, ID
 
@@ -34,26 +35,30 @@ const posts = [{
     id: '12',
     title: 'Programming Music',
     body: '',
-    published: false,
+    published: true,
     author: '2'
 }]
 
 const comments = [{
     id: '102',
     text: 'This worked well for me. Thanks!',
-    author: '3'
+    author: '3',
+    post: '10'
 }, {
     id: '103',
     text: 'Glad you enjoyed it.',
-    author: '1'
+    author: '1',
+    post: '10'
 }, {
     id: '104',
     text: 'This did no work.',
-    author: '2'
+    author: '2',
+    post: '11'
 }, {
     id: '105',
     text: 'Nevermind. I got it to work.',
-    author: '1'
+    author: '1',
+    post: '11'
 }]
 
 // Type definitions (schema)
@@ -64,6 +69,31 @@ const typeDefs = `
         comments: [Comment!]!
         me: User!
         post: Post!
+    }
+
+    type Mutation {
+        createUser(data: CreateUserInput!): User!
+        createPost(data: CreatePostInput!): Post!
+        createComment(data: CreateCommentInput!): Comment!
+    }
+
+    input CreateUserInput {
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    input CreatePostInput {
+        title: String!
+        body: String!
+        published: Boolean!
+        author: ID!
+    }
+
+    input CreateCommentInput {
+        text: String!
+        author: ID!
+        post: ID!
     }
 
     type User {
@@ -81,12 +111,14 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
+        comments: [Comment!]!
     }
 
     type Comment {
         id: ID!
         text: String!
         author: User!
+        post: Post!
     }
 `
 
@@ -132,10 +164,66 @@ const resolvers = {
             }
         }
     },
+    Mutation: {
+        createUser(parent, args, ctx, info) {
+            const emailTaken = users.some((user) => user.email === args.data.email)
+
+            if (emailTaken) {
+                throw new Error('Email taken')
+            }
+            
+            const user = {
+                id: uuidv4(),
+                ...args.data
+            }
+
+            users.push(user)
+
+            return user
+        },
+        createPost(parent, args, ctx, info) {
+            const userExists = users.some((user) => user.id === args.data.author)
+
+            if (!userExists) {
+                throw new Error('User not found')
+            }
+
+            const post = {
+                id: uuidv4(),
+                ...args.data
+            }
+
+            posts.push(post)
+
+            return post
+        },
+        createComment(parent, args, ctx, info){
+            const userExists = users.some((user) => user.id === args.data.author)
+            const postExists = posts.some((post) => post.id === args.data.post && post.published)
+
+            if (!userExists || !postExists) {
+                throw new Error('Unable to find user and post')
+            }
+
+            const comment = {
+                id: uuidv4(),
+                ...args.data
+            }
+
+            comments.push(comment)
+
+            return comment
+        }
+    },
     Post: {
         author(parent, args, ctx, info) {
             return users.find((user) => {
                 return user.id === parent.author
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.post === parent.id
             })
         }
     },
@@ -143,6 +231,11 @@ const resolvers = {
         author(parent, args, ctx, info) {
             return users.find((user) => {
                 return user.id === parent.author
+            })
+        },
+        post(parent, args, ctx, info) {
+            return posts.find((post) => {
+                return post.id === parent.post
             })
         }
     },
@@ -166,5 +259,5 @@ const server = new GraphQLServer({
 })
 
 server.start(() => {
-    console.log('The server is up and running on port 4000')
+    console.log('The server is up and running!')
 })
